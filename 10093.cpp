04a7c52db9b5ce52f00c2fc99973ae7d70142578
pha-ran <pha-ran@naver.com>
2021-07-04 00:02:03 +0900

    10093

---
+++ 10093.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long a, b;
+    cin >> a >> b;
+    if (a > b) swap(a, b);
+    if (a == b || b - a == 1) cout << 0;
+    else {
+        cout << b - a - 1 << "\n";
+        while (++a < b) cout << a << " ";
+    }
+}
